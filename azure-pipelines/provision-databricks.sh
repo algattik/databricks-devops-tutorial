b8bef9b36fa089972ca622e36b86d23cf0a08a3b
algattik <algattik@users.noreply.github.com>
2019-04-08 23:32:37 +0200

    Remove unneeded cluster creation

--- azure-pipelines/provision-databricks.sh
+++ azure-pipelines/provision-databricks.sh
@@ -13,28 +13,3 @@ databricks secrets write --scope bikeshare --key storagekey --string-value $stor
 databricks fs cp --overwrite java-library/target/*.jar dbfs:/model-factory.jar
 
 
-if databricks clusters list  --output JSON| jq -e '.clusters[] | select (.cluster_name == "small")' ; then
-	exit
-fi
-
-
-cluster=$(databricks clusters create --json '{
-  "cluster_name": "small",
-  "spark_version": "5.2.x-scala2.11",
-  "node_type_id": "Standard_DS3_v2",
-  "autoscale" : {
-    "min_workers": 1,
-    "max_workers": 3
-  },
-  "spark_env_vars": {
-    "PYSPARK_PYTHON": "/databricks/python3/bin/python3"
-  },
-  "autotermination_minutes": 120
-}')
-
-
-cluster_id=$(echo $cluster | jq -r .cluster_id)
-
-databricks libraries install  --cluster-id $cluster_id --jar dbfs:/model-factory.jar
-
-
